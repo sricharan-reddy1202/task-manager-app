b6557e91847efade2dc2e8f402aab6233eecdd3a
Sricharan Reddy <sricharan1202@gmail.com>
2026-03-12 07:17:00 +0530

    Implemented login API with JWT authentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
-
+const jwt = require("jsonwebtoken");
 exports.registerUser = async (req, res) => {
     try {
 
@@ -23,6 +23,38 @@ exports.registerUser = async (req, res) => {
         res.status(201).json({
             message: "User registered successfully",
             user
+        });
+
+    } catch (error) {
+        res.status(500).json({ message: "Server error" });
+    }
+};
+exports.loginUser = async (req, res) => {
+    try {
+
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        const token = jwt.sign(
+            { id: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: "1d" }
+        );
+
+        res.json({
+            message: "Login successful",
+            token
         });
 
     } catch (error) {
