f8dcb1e96942c8d3e8923d990ac64ee5995881b5
Sricharan Reddy <sricharan1202@gmail.com>
2026-03-14 06:13:12 +0530

    Implemented delete task API with authorization check

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -88,3 +88,24 @@ exports.updateTask = async (req, res) => {
         res.status(500).json({ message: "Server error" });
     }
 };
+exports.deleteTask = async (req, res) => {
+    try {
+
+        const task = await Task.findById(req.params.id);
+
+        if (!task) {
+            return res.status(404).json({ message: "Task not found" });
+        }
+
+        if (task.createdBy.toString() !== req.user._id.toString()) {
+            return res.status(401).json({ message: "Not authorized" });
+        }
+
+        await task.deleteOne();
+
+        res.json({ message: "Task deleted successfully" });
+
+    } catch (error) {
+        res.status(500).json({ message: "Server error" });
+    }
+};
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { createTask,getTasks, getTaskById, updateTask } = require("../controllers/taskController");
+const { createTask,getTasks, getTaskById, updateTask,deleteTask} = require("../controllers/taskController");
 const protect = require("../middleware/authMiddleware");
 
 router.post("/", protect, createTask);
 router.get("/", protect, getTasks);
 router.get("/:id", protect, getTaskById);
 router.put("/:id", protect, updateTask);
+router.delete("/:id", protect, deleteTask);
 module.exports = router;
