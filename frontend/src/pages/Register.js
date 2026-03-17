105635ccb20fa965ecf7f6c4c0130dede4b3e1e2
Sricharan Reddy <sricharan1202@gmail.com>
2026-03-17 07:23:28 +0530

    Implemented register page with API integration

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,7 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import { registerUser } from "../services/authService";
 
 function Register() {
-  return <h2>Register Page</h2>;
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: ""
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const data = await registerUser(formData);
+      console.log(data);
+      alert("User registered successfully");
+    } catch (error) {
+      console.log("ERROR:", error);
+      console.log("RESPONSE:", error.response);
+
+  alert(error.response?.data?.message || error.message);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
 }
 
 export default Register;
---
+++ frontend/src/services/authService.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:5000/api/auth";
+
+export const registerUser = async (userData) => {
+  const response = await axios.post(`${API_URL}/register`, userData);
+  return response.data;
+};
