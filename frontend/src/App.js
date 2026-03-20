b1430f7b08d643c84a7d98eb78b4d47e92d2f1c8
Sricharan Reddy <sricharan1202@gmail.com>
2026-03-20 10:25:42 +0530

    css added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-
+import "./index.css";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
